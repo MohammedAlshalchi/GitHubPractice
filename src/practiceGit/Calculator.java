663cbb2c332c1bd2c04ab4caf9a2f48fcad6062b
Mohammed <95448569+MohammedAlshalchi@users.noreply.github.com>
2022-01-18 19:47:33 -0500

    create multiply method

--- src/practiceGit/Calculator.java
+++ src/practiceGit/Calculator.java
@@ -3,5 +3,26 @@ package practiceGit;
 public class Calculator {
     public static void main(String[] args) {
 
+
+
     }
+    public static int add (int num1 , int num2){
+
+        int result = num1 + num2;
+        return result;
+    }
+    public static int subtract (int num1 , int num2){
+        int result = num1-num2;
+        return result;
+    }
+    public static int divide ( int num1 , int num2){
+        int result = num1/num2;
+        return result;
+    }
+    public static int multiply ( int num1 , int num2){
+        int result = num1*num2;
+        return result;
+    }
+
+
 }
